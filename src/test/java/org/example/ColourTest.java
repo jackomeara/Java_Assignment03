f8e3253e73be4cf0a6c6078559ed5672817d1cf7
jackom <120406576@umail.ucc.ie>
2022-12-02 12:57:42 +0000

    created red test to check illegal float values for three float constructor (not in 0.0<x<1.0)

--- src/test/java/org/example/ColourTest.java
+++ src/test/java/org/example/ColourTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ColourTest {
 
     @Test
-    void ConstructorWithThreeFloatValues() {
+    void ConstructorWithThreeFloatRGBValues() {
         float r = 0.8F;
         float g = 0.4F;
         float b = 0.2376548F;
@@ -17,5 +17,16 @@ class ColourTest {
         assertEquals(r, colour.getR());
         assertEquals(g, colour.getG());
         assertEquals(b, colour.getB());
+    }
+
+    @Test
+    void ConstructorWithIllegalFloatRGBValuesOutsideRange(){
+        float r = 1.1F;
+        float g = 0.5436F;
+        float b = -0.01F;
+
+        assertThrows(IllegalArgumentException.class, ()->{
+            Colour colour = new Colour(r, g, b);
+        });
     }
 }
