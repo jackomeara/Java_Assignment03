e2f4618e7b3b154f78b6b22165b0c15eb3aa30ad
jackom <120406576@umail.ucc.ie>
2022-12-02 12:45:01 +0000

    implemented 3 float constructor and getters to pass test

--- src/main/java/org/example/Colour.java
+++ src/main/java/org/example/Colour.java
@@ -1,7 +1,43 @@
 package org.example;
 
 public class Colour {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private float r;
+    private float g;
+    private float b;
+
+    /**
+     * Constructor for colour with three float values
+     * @param r float to represent red value
+     * @param g float to represent green value
+     * @param b float to represent blue value
+     */
+    public Colour(float r, float g, float b){
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    /**
+     * Getter for red value
+     * @return r red value as a float
+     */
+    public float getR(){
+        return r;
+    }
+
+    /**
+     * Getter for green value
+     * @return g green value as a float
+     */
+    public float getG(){
+        return g;
+    }
+
+    /**
+     * Getter for blue value
+     * @return b blue value as a float
+     */
+    public float getB(){
+        return b;
     }
 }
