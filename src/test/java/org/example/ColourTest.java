497b32a371403279d82b9ebdad43adf44ba9aa0b
jackom <120406576@umail.ucc.ie>
2022-12-02 12:39:35 +0000

    red test for constructor with 3 float values

--- src/test/java/org/example/ColourTest.java
+++ src/test/java/org/example/ColourTest.java
@@ -1,10 +1,21 @@
 package org.example;
+import org.junit.jupiter.api.Test;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ColourTest {
 
-    @org.junit.jupiter.api.Test
-    void main() {
+    @Test
+    void ConstructorWithThreeFloatValues() {
+        float r = 0.8F;
+        float g = 0.4F;
+        float b = 0.2376548F;
+
+        Colour colour = new Colour(r, g, b);
+
+        assertEquals(r, colour.getR());
+        assertEquals(g, colour.getG());
+        assertEquals(b, colour.getB());
     }
 }
